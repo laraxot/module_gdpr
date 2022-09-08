88e15dd50ac25542073f61f1671806ca96655d4d
marco xot <marco.sottana@gmail.com>
2022-09-08 17:19:10 +0000

    up

--- Database/Seeders/GdprDatabaseSeeder.php
+++ Database/Seeders/GdprDatabaseSeeder.php
@@ -1,19 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Gdpr\Database\Seeders;
 
-use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
 
-class GdprDatabaseSeeder extends Seeder
-{
+class GdprDatabaseSeeder extends Seeder {
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
         Model::unguard();
 
         // $this->call("OthersTableSeeder");
--- Routes/web.php
+++ Routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +13,6 @@
 |
 */
 
-Route::prefix('gdpr')->group(function() {
+Route::prefix('gdpr')->group(function () {
     Route::get('/', 'GdprController@index');
 });
