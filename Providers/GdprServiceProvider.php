cb9e023b5d630320b5c57f247428aac9a599d4f1
Marco <marco.sottana@gmail.com>
2023-01-18 12:30:53 +0100

    up

--- Providers/GdprServiceProvider.php
+++ Providers/GdprServiceProvider.php
@@ -6,7 +6,8 @@ namespace Modules\Gdpr\Providers;
 
 use Modules\Xot\Providers\XotBaseServiceProvider;
 
-class GdprServiceProvider extends XotBaseServiceProvider {
+class GdprServiceProvider extends XotBaseServiceProvider
+{
     protected string $module_dir = __DIR__;
     protected string $module_ns = __NAMESPACE__;
     public string $module_name = 'gdpr';
